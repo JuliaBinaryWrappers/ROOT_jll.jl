f5c06a3d9333a97a128ad20d22310bf5fa283397
jlbuild <juliabuildbot@gmail.com>
2025-06-29 22:50:26 +0000

    ROOT_jll build 6.32.8+1

--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -4,6 +4,7 @@ export root, rootcling, rootcling_stage1
 using Xorg_libX11_jll
 using Xorg_libXpm_jll
 using Xorg_libXft_jll
+using Libtiff_jll
 using VDT_jll
 using XRootD_jll
 using Lz4_jll
@@ -37,7 +38,7 @@ JLLWrappers.@declare_executable_product(root)
 JLLWrappers.@declare_executable_product(rootcling)
 JLLWrappers.@declare_executable_product(rootcling_stage1)
 function __init__()
-    JLLWrappers.@generate_init_header(Xorg_libX11_jll, Xorg_libXpm_jll, Xorg_libXft_jll, VDT_jll, XRootD_jll, Lz4_jll, FFTW_jll, Giflib_jll, Zstd_jll, PCRE2_jll, Graphviz_jll, xxHash_jll, XZ_jll, Librsvg_jll, FreeType2_jll, Xorg_libICE_jll, Xorg_libSM_jll, Xorg_libXfixes_jll, Xorg_libXi_jll, Xorg_libXinerama_jll, Xorg_libXmu_jll, Xorg_libXt_jll, Xorg_libXtst_jll, Xorg_xcb_util_jll, Xorg_libxkbfile_jll, Libglvnd_jll, GLU_jll, GLEW_jll, CFITSIO_jll, oneTBB_jll, OpenBLAS32_jll)
+    JLLWrappers.@generate_init_header(Xorg_libX11_jll, Xorg_libXpm_jll, Xorg_libXft_jll, Libtiff_jll, VDT_jll, XRootD_jll, Lz4_jll, FFTW_jll, Giflib_jll, Zstd_jll, PCRE2_jll, Graphviz_jll, xxHash_jll, XZ_jll, Librsvg_jll, FreeType2_jll, Xorg_libICE_jll, Xorg_libSM_jll, Xorg_libXfixes_jll, Xorg_libXi_jll, Xorg_libXinerama_jll, Xorg_libXmu_jll, Xorg_libXt_jll, Xorg_libXtst_jll, Xorg_xcb_util_jll, Xorg_libxkbfile_jll, Libglvnd_jll, GLU_jll, GLEW_jll, CFITSIO_jll, oneTBB_jll, OpenBLAS32_jll)
     JLLWrappers.@init_executable_product(
         root,
         "bin/root",
