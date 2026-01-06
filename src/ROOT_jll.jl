8cac15e90b1632cbac27a170ae9c94f98af8b623
jlbuild <juliabuildbot@gmail.com>
2026-01-06 11:22:36 +0000

    ROOT_jll build 6.32.8+2

--- src/ROOT_jll.jl
+++ src/ROOT_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("ROOT")
-JLLWrappers.@generate_main_file("ROOT", UUID("45b42145-bbac-5752-8807-01f8b2702242"))
+JLLWrappers.@generate_main_file("ROOT", Base.UUID("45b42145-bbac-5752-8807-01f8b2702242"))
 end  # module ROOT_jll
